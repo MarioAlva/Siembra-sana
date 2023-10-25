9e74be3b3752b916dc7f449ac4508e22982c8a49
MarioAlva <marioantonioalva@gmail.com>
2023-10-26 00:14:17 +0200

    Correct mail

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,9 +15,10 @@ export default function Contact() {
 					<section className="flex justify-center w-full">
 						<form action="contact" className="flex flex-col w-[1200px] max-w-full px-12 mb-12">
 							<label className="text-sm text-neutral-600 mt-2" htmlFor="name">Nombre</label>
-							<input onChange={(e) => setName(e.currentTarget.value)} className="text-sm w-full py-1 px-2 border border-neutral-600 focus:outline-green-600 focus:outline" value={name} type="text" name="name" id="name" placeholder="Nombre" />							<label className="text-sm text-neutral-600 mt-2" htmlFor="message">Mensaje</label>
+							<input onChange={(e) => setName(e.currentTarget.value)} className="text-sm w-full py-1 px-2 border border-neutral-600 focus:outline-green-600 focus:outline" value={name} type="text" name="name" id="name" placeholder="Nombre" />
+							<label className="text-sm text-neutral-600 mt-2" htmlFor="message">Mensaje</label>
 							<textarea onChange={(e) => setMessage(e.currentTarget.value)} className="text-sm w-full py-1 px-2 border border-neutral-600 focus:outline-green-600 focus:outline" value={message} name="message" id="message" cols={30} rows={10} placeholder="Mensaje"></textarea>
-							<a href={"mailto:marioantonioalva@gmail.com?subject=Nuevo mensaje de " + name + "&body=" + message} className="cursor-pointer font-bold w-full bg-green-600 py-2 mt-4 rounded text-white focus:outline-2 focus:outline-green-800 flex justify-center items-center" >Enviar</a>
+							<a href={"mailto:comercial@siembrasana.bio?subject=Nuevo mensaje de " + name + "&body=" + message} className="cursor-pointer font-bold w-full bg-green-600 py-2 mt-4 rounded text-white focus:outline-2 focus:outline-green-800 flex justify-center items-center" >Enviar</a>
 						</form>
 					</section>
 				</div>
